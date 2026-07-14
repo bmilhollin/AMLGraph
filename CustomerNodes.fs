5244404266fa59bb7c316aa38d30274b5313e7fe
bmilhollin <bmilhollin@gmail.com>
2026-07-14 10:49:17 -0400

    refactor for simplicity

--- CustomerNodes.fs
+++ CustomerNodes.fs
@@ -2,39 +2,43 @@ namespace AMLGraph
 
 module CustomerNode =
 
+    let private toParameters (customer:Customer) =
+        dict [
+            "customerId", box customer.CustomerId
+            "firstName", box customer.FirstName
+            "lastName", box customer.LastName
+            "dob", box customer.DOB
+            "occupation", box customer.Occupation
+            "riskRating", box customer.RiskRating
+        ]
+
+
     let create (customers:Customer list) =
+
+        let cypher =
+            """
+            MERGE (c:Customer {customerId:$customerId})
+            SET
+                c.firstName = $firstName,
+                c.lastName = $lastName,
+                c.dob = $dob,
+                c.occupation = $occupation,
+                c.riskRating = $riskRating
+            """
 
         task {
 
             use session = Neo4j.driver.AsyncSession()
 
-            let cypher =
-                """
-                MERGE (c:Customer {customerId:$customerId})
-                SET
-                    c.firstName = $firstName,
-                    c.lastName = $lastName,
-                    c.dob = $dob,
-                    c.occupation = $occupation,
-                    c.riskRating = $riskRating
-                """
-            
             for customer in customers do
 
                 do!
                     session.ExecuteWriteAsync(
                         fun tx ->
                             task {
+
                                 let parameters =
-                                    // Neo4j expects dictionary<string, obj> for parameters
-                                    dict [
-                                        "customerId", box customer.CustomerId
-                                        "firstName", box customer.FirstName
-                                        "lastName", box customer.LastName
-                                        "dob", box customer.DOB
-                                        "occupation", box customer.Occupation
-                                        "riskRating", box customer.RiskRating
-                                    ]
+                                    toParameters customer
 
                                 let! _ =
                                     tx.RunAsync(
